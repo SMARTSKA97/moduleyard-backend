8fc1bc0eb5d41d1be776867ae0c6a9587175855f
SKA97 <karmakarsubhrajit680@gmail.com>
2026-03-01 19:14:53 +0530

    feat: Establish initial project structure and dependencies for the ModuleYard backend solution.

---
+++ Domain/Entities/ServerMetric.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain.Entities;
+
+public partial class ServerMetric
+{
+    public Guid Id { get; set; }
+
+    public string ServerName { get; set; } = null!;
+
+    public decimal CpuUsagePercentage { get; set; }
+
+    public decimal MemoryUsageMb { get; set; }
+
+    public DateTime RecordedAt { get; set; }
+}
---
+++ Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Domain.Entities;
+
+namespace Infrastructure.Data;
+
+public partial class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext()
+    {
+    }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<ServerMetric> ServerMetrics { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ServerMetric>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("server_metrics_pkey");
+
+            entity.ToTable("server_metrics", "sysmonitor");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("id");
+            entity.Property(e => e.CpuUsagePercentage)
+                .HasPrecision(5, 2)
+                .HasColumnName("cpu_usage_percentage");
+            entity.Property(e => e.MemoryUsageMb)
+                .HasPrecision(10, 2)
+                .HasColumnName("memory_usage_mb");
+            entity.Property(e => e.RecordedAt)
+                .HasDefaultValueSql("now()")
+                .HasColumnName("recorded_at");
+            entity.Property(e => e.ServerName)
+                .HasMaxLength(255)
+                .HasColumnName("server_name");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
